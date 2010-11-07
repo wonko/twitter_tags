e2b3beb910fe0e18d5edb3290750c72c10aab146
Matthew <pelargir@gmail.com>
2010-11-07 18:52:35 -0500

    added formatted twitter text tag

--- lib/twitter_tags.rb
+++ lib/twitter_tags.rb
@@ -1,5 +1,11 @@
 module TwitterTags
   include Radiant::Taggable
+  
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::UrlHelper
+  
+  include Rack::Utils
+  alias_method :h, :escape_html
 
   desc "Creates an context for the twitter functionality" 
   tag "twitter" do |tag|
@@ -37,7 +43,46 @@ module TwitterTags
     tweet = tag.locals.tweet
     tweet['text']
   end
-
+  
+  desc "Returns the text from the tweet with hyperlinked usernames and URLs"
+  tag "twitter:tweets:tweet:formatted_text" do |tag|
+    tweet = tag.locals.tweet
+    format_tweet tweet['text']
+  end
+  
+  def format_tweet(text)
+    text = html_escape(text)
+    text = link_urls(text)
+    text = link_usernames(text)
+    text = link_hashtags(text)
+    text = format_newlines(text)
+    text
+  end
+  
+  def link_hashtags(text)
+    text.gsub(/(^|\s|\(|\)\[|\]\{|\})\#([^\(\)\[\]\{\}\:\s]+)/) do
+      $1 + link_to("##{ $2 }", "http://twitter.com/search?q=%23#{ escape($2) }", :class=>"twitter_hashtag", :target=>"_blank")
+    end
+  end
+  
+  def link_usernames(text)
+    text.gsub(/(^|\s|\(|\)\[|\]\{|\})@([^\(\)\[\]\{\}\:\s]+)/) do
+      $1 + link_to("@#{ $2 }", "http://twitter.com/#{ escape($2) }", :class=>"twitter_username", :target=>"_blank")
+    end
+  end
+  
+  def link_urls(text)
+    text.gsub(%r{(http://[^\(\)\[\]\{\}\s]+?)($|[.;:\)\]\}] | )}i) do
+      link_to(escape_html($1), $1) + $2
+    end
+  end
+  
+  def format_newlines(text)
+    text = text.gsub(/\n/, "<br />")
+    text = text.gsub(%r{<br />\s*<br />}, "</p><p>")
+    "<p>#{text}</p>"
+  end
+  
   desc "Returns the date & time from the tweet"
   tag "twitter:tweets:tweet:date" do |tag|
     tweet = tag.locals.tweet
